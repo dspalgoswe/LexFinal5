b161482e04577178b9eb9ad790d2c56de09ab2fb
DZM77 <32109758+DZM77@users.noreply.github.com>
2025-01-09 13:50:55 +0100

    Add LMSContextFactory

---
+++ LMS.Infrastructure/Data/LMSContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Infrastructure.Data;
+public class LMSContextFactory : IDesignTimeDbContextFactory<LmsContext>
+{
+    public LmsContext CreateDbContext(string[] args)
+    {
+        var basePath = Directory.GetParent(Directory.GetCurrentDirectory())?.FullName;
+        ArgumentNullException.ThrowIfNull(nameof(basePath));
+
+        var configuration = new ConfigurationBuilder()
+                                    .SetBasePath(Path.Combine(basePath!, "LMS.API")) 
+                                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                                    .Build();
+
+        var builder = new DbContextOptionsBuilder<LmsContext>().UseSqlServer(configuration.GetConnectionString("LmsContext"));
+
+        return new LmsContext(builder.Options);
+    }
+}
+
