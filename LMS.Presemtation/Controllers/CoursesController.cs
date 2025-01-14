70243566f39439f3596f08e3b8a0c96da21e1c52
Reza Zam <rezazam_skol@outlook.com>
2025-01-14 10:20:01 +0100

    Student see the courses registred on, Created Controllers for student and courses.

---
+++ LMS.Presemtation/Controllers/CoursesController.cs
@@ -0,0 +1,35 @@
+﻿using LMS.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Presemtation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CoursesController : ControllerBase
+    {
+        private readonly LmsContext _context;
+
+        public CoursesController(LmsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{courseId}/participants")]
+        public async Task<IActionResult> GetCourseParticipants(int courseId)
+        {
+            //Hämtar deltagare som är kopplade till kursen
+            var paticipants = await _context.Users
+                .Include(c => c.Courses)
+                        //.ThenInclude(m => m.Activities)
+                        //.ThenInclude(a => a.ActivityType)
+                        //.Include(c => c.Users)
+                        //.Include(c => c.Documents)
+                        .ToListAsync();
+
+
+            return Ok(paticipants);
+        }
+
+    }
+}
---
+++ LMS.Presemtation/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using LMS.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Presemtation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly LmsContext _context;
+
+        public StudentController(LmsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{studentId}/course")]
+        public async Task<IActionResult> GetStudentCourse(string studentId)
+        {
+            var course = await _context.Courses.Include(c => c.Users)
+                        .FirstOrDefaultAsync(c => c.Users.Any(u => u.Id == studentId));
+
+            if (course == null)
+            {
+                return NotFound("Student is not registered in any of courses");
+            }
+
+            return Ok(
+                new
+                {
+                    course.CourseId,
+                    course.Name,
+                    course.Description,
+                    Modules = course.Modules.Select(m => new
+                    {
+                        m.Name,
+                        m.StartDate,
+                        m.EndDate
+                    })
+
+                });
+        }
+
+       
+
+    }
+}
